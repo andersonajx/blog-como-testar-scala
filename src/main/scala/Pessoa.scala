cfa9afa02dd4d85384b4d27b7030eb72afeb59b1
Anderson Clayton <andersonajx@gmail.com>
2013-10-21 00:38:50 -0200

    Feito a classe Pessoa e seu teste

--- src/main/scala/Pessoa.scala
+++ src/main/scala/Pessoa.scala
@@ -1,10 +1,9 @@
-import java.util.{Date, Calendar}
+import java.util.Calendar
 
 case class Pessoa(nome: String, nascimento: Calendar) {
 
   def calcularIdade = {
     val hoje = Calendar.getInstance()
-    hoje.setTime(new Date)
     hoje.get(Calendar.YEAR) - nascimento.get(Calendar.YEAR)
   }
 
